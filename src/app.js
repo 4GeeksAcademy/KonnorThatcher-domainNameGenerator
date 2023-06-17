2d2b19b840cff747fa50ba2b59311070f1a2785f
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-06-17 00:48:13 +0000

    Completed. Might make some tweaks.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let p of pronouns) {
+    for (let a of adj) {
+      for (let n of nouns) {
+        for (let x of ext) {
+          console.log(`${[p,a,n,x].join('')}`);
+          //document.querySelector("#output").innerHTML += `${[p,a,n,x].join('')} <br>`; // This line prints the generated domains on to the HTML page. Why don't you uncomment(?) it?
+        }
+      }
+    }
+  }
+  
 };
+
+let pronouns = ["the", "our", "them"];
+let adj = ["great", "epic", "fantastic", "scrumptious"];
+let nouns = ["racoon", "win", "khakis"];
+let ext = [".com", ".de", ".net", ".gov", ".io"]
